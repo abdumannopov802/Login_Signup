0399dc1ff670b935c46d9ce94492f1766237c379
abdumannopov802 <154593414+abdumannopov802@users.noreply.github.com>
2024-03-02 20:56:29 +0500

    Initial commit

---
+++ accounts/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout 
+from .forms import UserCreationForm, LoginForm
+
+
+# Create your views here.
+
+# Home page
+def index(request):
+    return render(request, 'home.html')
+
+# signup page
+def user_signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'sign-up.html', {'form': form})
+
+# login page
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)    
+                return redirect('home')
+    else:
+        form = LoginForm()
+    return render(request, 'log-in.html', {'form': form})
+
+# logout page
+def user_logout(request):
+    logout(request)
+    return redirect('login')
